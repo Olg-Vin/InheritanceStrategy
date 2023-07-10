42835e85845ab0914cc27f308c79ffd0e2d019b0
Vinio <Ewq@1605>
2023-07-10 23:29:30 +0300

    fill class truck

--- src/main/java/summerPractice/modelsFirst/Truck.java
+++ src/main/java/summerPractice/modelsFirst/Truck.java
@@ -3,8 +3,33 @@ package summerPractice.modelsFirst;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "truck")
 public class Truck extends Vehicle{
     protected Double loadCapacity;
+
+    public Truck() {
+    }
+
+    public Truck(String type, String model, BigDecimal price, String fuelType, Double loadCapacity) {
+        super(type, model, price, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public Double getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    protected void setLoadCapacity(Double loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Truck{" +
+                "loadCapacity=" + loadCapacity +
+                '}';
+    }
 }
